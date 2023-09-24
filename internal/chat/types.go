4bf106f03f2d58487242a89cc2c8c67dac141f91
Alex Forsythe <awforsythe@gmail.com>
2023-09-24 13:06:21 -0400

    Format emote URLs from IDs, based on static Twitch CDN URL template

--- internal/chat/types.go
+++ internal/chat/types.go
@@ -49,14 +49,7 @@ type Deletion struct {
 // newMessageEvent constructs an Event with type 'message' given an IRC PRIVMSG, or nil
 // if the IRC message should not result in a new chat line being displayed
 func newMessageEvent(m *irc.PrivateMessage) *Event {
-	text, emoteNames := substituteEmotes(m)
-	emotes := make([]EmoteDetails, 0, len(emoteNames))
-	for _, emoteName := range emoteNames {
-		emotes = append(emotes, EmoteDetails{
-			Name: emoteName,
-			Url:  "",
-		})
-	}
+	text, emotes := substituteEmotes(m)
 	return &Event{
 		Type: EventTypeMessage,
 		Message: &Message{
@@ -69,16 +62,19 @@ func newMessageEvent(m *irc.PrivateMessage) *Event {
 	}
 }
 
-func substituteEmotes(m *irc.PrivateMessage) (string, []string) {
+func substituteEmotes(m *irc.PrivateMessage) (string, []EmoteDetails) {
 	tokens := strings.Split(strings.ReplaceAll(m.Message, "$", "$$"), " ")
-	emoteNames := make([]string, 0, len(m.Emotes))
+	emotes := make([]EmoteDetails, 0, len(m.Emotes))
 	for emoteIndex, emote := range m.Emotes {
 		for tokenIndex := 0; tokenIndex < len(tokens); tokenIndex++ {
 			if tokens[tokenIndex] == emote.Name {
 				tokens[tokenIndex] = fmt.Sprintf("$%d", emoteIndex)
 			}
 		}
-		emoteNames = append(emoteNames, emote.Name)
+		emotes = append(emotes, EmoteDetails{
+			Name: emote.Name,
+			Url:  fmt.Sprintf("https://static-cdn.jtvnw.net/emoticons/v2/%s/default/dark/1.0", emote.ID),
+		})
 	}
-	return strings.Join(tokens, " "), emoteNames
+	return strings.Join(tokens, " "), emotes
 }
