3224e4650a51519549cb968da53625c3eed40da0
Alex Forsythe <awforsythe@gmail.com>
2023-09-29 21:25:50 -0400

    Set first_logged_in_at if viewer record exists but has no prior logins

--- gen/queries/viewer.sql.go
+++ gen/queries/viewer.sql.go
@@ -48,6 +48,7 @@ insert into showtime.viewer (
 )
 on conflict (twitch_user_id) do update set
     twitch_display_name = excluded.twitch_display_name,
+    first_logged_in_at = coalesce(viewer.first_logged_in_at, excluded.first_logged_in_at),
     last_logged_in_at = excluded.last_logged_in_at
 `
 
